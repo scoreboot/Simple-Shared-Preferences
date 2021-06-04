d3899f661f79d2795c7ef6de5ecd1962b8f2e09e
scoreboot <info@scorega.me>
2021-06-04 23:25:30 +0300

    --

--- SimpleData/src/main/java/com/scoreboot/simpledata/SPData.java
+++ SimpleData/src/main/java/com/scoreboot/simpledata/SPData.java
@@ -6,6 +6,8 @@ import android.content.SharedPreferences;
 
 import androidx.preference.PreferenceManager;
 
+import java.util.Set;
+
 public class SPData {
     private static SharedPreferences prefs = null;
     private static SharedPreferences.Editor prefsEditor = null;
@@ -50,4 +52,37 @@ public class SPData {
 
         return prefs.getString(key, def);
     }
+    public static void setLong(String key, long value) {
+
+        prefsEditor.putLong(key, value);
+        prefsEditor.apply();
+    }
+
+    public static long getLong(String key, long def) {
+
+        return prefs.getLong(key, def);
+    }
+
+    public static void setFloat(String key, float value) {
+
+        prefsEditor.putFloat(key, value);
+        prefsEditor.apply();
+    }
+
+    public static float getFloat(String key, float def) {
+
+        return prefs.getFloat(key, def);
+    }
+
+    public static void setStringSet(String key, Set<String> value) {
+
+        prefsEditor.putStringSet(key, value);
+        prefsEditor.apply();
+    }
+
+    public static Set<String> getStingSet(String key, Set<String> def) {
+
+        return prefs.getStringSet(key, def);
+    }
+
 }
